114e37c1b83eb19485fadf97231634ba92db6998
abaument <arthurbaument@gmail.com>
2023-12-11 15:35:26 +0100

    Initial Commit

---
+++ MingledOfficial/MingledOfficialApp.swift
@@ -0,0 +1,17 @@
+//
+//  MingledOfficialApp.swift
+//  MingledOfficial
+//
+//  Created by arthur baument on 11/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct MingledOfficialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
